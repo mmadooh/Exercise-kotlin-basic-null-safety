249a755ed98535eebdb7d321b3b8738de4e4c759
Mohamed Madooh <mmadooh14@gmail.com>
2025-02-18 17:47:29 +0300

    done

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,19 @@
 fun main() {
-    println("Hello World!")
+
+    var age: Int = 0
+    var cityName: String? = "New York"
+    cityName = null
+    println(cityName?.length)
+    val nameLength = cityName?.length ?: 0
+    println(nameLength)
+    var score = null
+    printScoreDec(score)
+
+}
+
+fun printScoreDec(score: Double?) {
+    val length = score?.dec() ?: "No score provided"
+    if (score != null) {
+        println("The score is ${score.dec()}")
+    }
 }
